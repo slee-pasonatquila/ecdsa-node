10465e1ede811a71b6cb5c3dcc0b2556b5465c47
s.Lee <slee@circlace.com>
2023-04-04 17:12:51 +0900

    add logic

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,31 +1,36 @@
 import server from "./server";
+import * as secp from "ethereum-cryptography/secp256k1";
+import { keccak256 } from "ethereum-cryptography/keccak";
+import { utf8ToBytes } from "ethereum-cryptography/utils";
+import { toHex } from "ethereum-cryptography/utils";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
-  async function onChange(evt) {
-    const address = evt.target.value;
-    setAddress(address);
-    if (address) {
-      const {
-        data: { balance },
-      } = await server.get(`balance/${address}`);
-      setBalance(balance);
-    } else {
-      setBalance(0);
+function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
+    async function onChange(evt) {
+        const address = evt.target.value;
+        setAddress(address);
+        if (address) {
+            const {
+                data: { balance, privateKey },
+            } = await server.get(`balance/${address}`);
+            setBalance(balance);
+            setPrivateKey(privateKey);
+        } else {
+            setBalance(0);
+        }
     }
-  }
 
-  return (
-    <div className="container wallet">
-      <h1>Your Wallet</h1>
+    return (
+        <div className="container wallet">
+            <h1>Your Wallet</h1>
 
-      <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
-      </label>
+            <label>
+                Wallet address
+                <input placeholder="Type your address" value={address} onChange={onChange}></input>
+            </label>
 
-      <div className="balance">Balance: {balance}</div>
-    </div>
-  );
+            <div className="balance">Balance: {balance}</div>
+        </div>
+    );
 }
 
 export default Wallet;
